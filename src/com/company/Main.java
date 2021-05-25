7d8493c31fbab0b4e2291ab8322a1413bb9b1cb8
Ayashika11 <51939103+Ayashika11@users.noreply.github.com>
2021-05-25 19:00:01 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	int NoOfRows,NoOfColumns,treeNumber;
+	int NoOfRows,NoOfColumns,treeNumber,totalNoOftrees;
 	Scanner sc = new Scanner(System.in);
 	NoOfRows = sc.nextInt();
 	NoOfColumns = sc.nextInt();
 	treeNumber = sc.nextInt();
+	totalNoOftrees = NoOfColumns*NoOfRows;
 	if(treeNumber<=NoOfColumns || treeNumber%NoOfColumns==1 || treeNumber%NoOfColumns==0)
 	{
 		System.out.println("It is a mango tree.");
